e1d16ce16e5d12d6d206575d1c01a5f1089b8b3a
Riccardo Seppi <56032596+riccardo-seppi@users.noreply.github.com>
2019-10-30 16:32:15 +0100

    Add files via upload

--- MF_fit.py
+++ MF_fit.py
@@ -27,49 +27,24 @@ params = {'flat': False, 'H0': hubble*100, 'Om0': Omega0, 'Ode0': Omega0DE, 'Ob0
 print('reading masses from the catalog...')
 #masses = pd.read_table(catalog, usecols=[7], skiprows=7, sep=' ', dtype=float)
 masses = np.loadtxt(catalog, usecols=[7], skiprows=8, dtype=float)
+masses = masses/hubble #get reale masses instead of Msol/h
 masses.sort()
 print('Lowest mass: %.3g' %np.min(masses))
 print('Maximun mass: %.3g' %np.max(masses))
 
-#create mass bins
+#create mass bins and count halos in different bins
 nbins=100
-mass_bins=np.logspace(12.0,14.1,nbins)
-#create vectors that will contain the number of halos at a given mass
-mass_number = np.zeros(len(mass_bins))
-mass_average = np.zeros(len(mass_bins))
-
-#fill these vectors counting how many halos belong to different bins
-'''
-count=0
-counter=0
-for i in range(len(mass_bins)):
-    for j in range(count,len(masses)):
-        if(i==0):
-            if(masses[j]<=mass_bins[i]):
-                mass_number[i]=mass_number[i]+1
-                mass_average[i]=mass_average[i]+masses[j]
-                counter = counter+1
-        else:   
-            if(masses[j] <= mass_bins[i] and masses[j] > mass_bins[i-1]):
-                mass_number[i]=mass_number[i]+1
-                mass_average[i]=mass_average[i]+masses[j]
-                counter = counter+1
-        count = count+counter
-        counter = 0
-    print('cicle ',i,' of ', len(mass_bins))
-'''
-
-j=0 
-for i in range(len(masses)):
-    if(masses[i]<=mass_bins[j]):
-        mass_number[j]=mass_number[j]+1
-    else:
-        j=j+1
-        if(j==nbins):
-            break
-        i=i-1
-        print('cicle ',j,' of ', len(mass_bins))
+mass_number, mass_bins = np.histogram(masses,bins=np.logspace(12.2,14.1,nbins))
+plt.hist(masses,mass_bins)
+plt.loglog()
+plt.show()
     
+diff = np.diff(np.log(mass_bins))
+print(diff)
+diff=diff[0]
+
+
+print('mass_number length = ', len(mass_number), 'mass_bins length = ', len(mass_bins))
 print('number of halos in each bin: ',mass_number)   
 print('total number of halos used: ',np.sum(mass_number))
 print('total number of halos in the simulation: ', len(masses))
@@ -78,12 +53,17 @@ print('total number of halos in the simulation: ', len(masses))
 #the simulation is a cube of 1 Gpc/h...
 #divide the number by 10^9 to get the number per (Mpc/h)^3
 counts_error = np.sqrt(mass_number)/10**9
-mass_number = mass_number/(10**9)
+mass_number = mass_number/(10**9)/diff
 
    
 #now I can build the mass function
+#plot each data in the middle of its respective mass bin
+mass_bins_pl = np.zeros(len(mass_number))
+for i in range(len(mass_number)):
+	mass_bins_pl[i] = (mass_bins[i+1] + mass_bins[i])/2
+
 plt.figure()
-plt.errorbar(mass_bins,mass_number,yerr=counts_error, fmt='.', label='data')
+plt.errorbar(mass_bins_pl,mass_number,yerr=counts_error, fmt='.', label='data')
 plt.xscale('log')
 plt.yscale('log')
 plt.xlabel(r'$M_{\odot}$', fontsize=18)
@@ -96,11 +76,37 @@ plt.tight_layout()
 #NB: z will have to be the same of the simulation analyzed!!!
 from colossus.lss import mass_function as mf
 from colossus.cosmology import cosmology
+from colossus.lss import peaks
 #cosmology.setCosmology('planck18')
 cosmology.addCosmology('myCosmo', params) #params was defined at line 24
-cosmology.setCosmology('myCosmo')
-mass_func_model=mf.massFunction(mass_bins,z=z,mdef = 'vir', model = 'comparat17', q_out = 'dndlnM')
-plt.plot(mass_bins, mass_func_model, label='model')
+cosmo=cosmology.setCosmology('myCosmo')
+#print(cosmo.rho_m(0.0))
+
+#fitting with Bhattacharya 2011
+def mass_function_rseppi(Mass):
+	cosmo=cosmology.getCurrent()	
+	delta_c = peaks.collapseOverdensity(z=z)
+	R = peaks.lagrangianR(Mass)
+	sigma = cosmo.sigma(R=R,z=z)
+	nu = delta_c / sigma
+	nu2 = nu**2
+	zp1 = 1.0+z
+	A = 0.333 * zp1**-0.11
+	a = 0.788 * zp1**-0.01
+	p = 0.807
+	q = 1.795
+	f = A * np.sqrt(2 / np.pi) * np.exp(-a * nu2 * 0.5) * (1.0 + (nu2*a)**-p) * (nu * np.sqrt(a))**q
+
+	d_ln_sigma_d_ln_R = cosmo.sigma(R, z, derivative = True)
+	rho_Mpc = cosmo.rho_m(0.0) * 1E9
+	mass_func_model = -1/3*f*rho_Mpc/Mass*d_ln_sigma_d_ln_R
+	return mass_func_model
+
+
+mass_func_model = mass_function_rseppi(mass_bins_pl)
+print('model values = ', mass_func_model)
+#mass_func_model=mf.massFunction(mass_bins_pl,z=z,mdef = 'vir', model = 'tinker08', q_out = 'dndlnM')
+plt.plot(mass_bins_pl, mass_func_model, label='model')
 plt.show()
 
 #define residual function
